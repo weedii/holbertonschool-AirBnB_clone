ffaa2626594fe7958947ef26e9186b2cc4fa05cf
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-20 14:48:29 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,18 @@ import datetime
 class BaseModel():
     """BaseModel class the parent of other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initializing the public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if (kwargs):
+            for key, value in (kwargs.items()):
+                if (key != "__class__"):
+                    if (key == "created_at" or key == "updated_at"):
+                        value = datetime.datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
 
     # __str__ method
     def __str__(self):
