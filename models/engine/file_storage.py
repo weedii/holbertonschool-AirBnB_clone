f44059abb53fe97b46f21d614ab33a714fee3c03
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-21 21:15:34 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,48 @@
 #!/usr/bin/python3
-"""
 
 """
+FileStorage Module
+"""
+
 import json
-import os.path
-from .. import base_model
+import os
+from models.base_model import BaseModel
 
 
-class BaseModelEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, base_model.BaseModel):
-            return obj.to_dict()
-        return super().default(obj)
+class FileStorage():
+    """FileStorage  class that inherits from BaseModel class"""
 
-class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    # all method
     def all(self):
-        #A public instance method to return the dictionary of our objects
-        return(self.__objects)
+        """method that returns the dictionary __objects"""
+        return self.__objects
 
+    # new method
     def new(self, obj):
-        #gives the right representation of our obj in the dictionary objects
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
-    
-    def save(self):
-        #add our objects dictionary to the needed json file
-        with open(self.__file_path, "a+", encoding="utf-8") as f:
-            j = json.dumps(self.__objects, cls=BaseModelEncoder)
-            f.write(j)
+        """method that sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
 
+    # save method
+    def save(self):
+        """method that serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    # reload method
     def reload(self):
-        if os.path.isfile(self.__file_path) == False:
-            return
-        f = open(self.__file_path, "r", encoding="utf-8")
-        read = f.read()
-        if read:
-            lst = json.loads(read)
-            self.__objects = dict(lst)
+        """method that deserializes the JSON file"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj = json.loads(file.read())
+            for key, value in obj.items():
+                class_name, id = key.split('.')
+                self.__objects[key] = eval(class_name)(**value)
+        except BaseException:
+            pass
