afe27b4c83fa1496fdd029690a140879a5487585
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-22 16:21:16 +0100

    User Class

--- models/user.py
+++ models/user.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""
 
 """
+Class of User
+"""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """User Class that inherits from BaseModel"""
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    # __str__ method
     def __str__(self):
         """method that return string representation of an instance"""
         return f"[{User.__name__}] ({self.id}) {self.__dict__}"
